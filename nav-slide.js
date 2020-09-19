753ba5f99d288b89455f260a6b230187fda9d0c6
danny235 <danielbarima235@gmail.com>
2020-09-19 22:02:54 +0100

    update

--- nav-slide.js
+++ nav-slide.js
@@ -5,6 +5,8 @@ const btn = document.getElementById("burger");
 btn.addEventListener("click", () => {
     menu.style.display = "block";
     menu.classList.add("animationSlide");
+    menu.classList.remove("animationRemove");
+    menu.style.boxShadow = "0 10px 10px 400px rgba(163, 163, 163, 0.54)"
 });
 
 
@@ -13,6 +15,7 @@ document.addEventListener("click", (e) => {
     // console.log(`clientX:${e.clientX} clientY:${e.clientY}`);
     if ((e.clientX) >= 264 && (e.clientY) >= 13) {
         menu.classList.remove("animationSlide");
-        menu.style.display = "none";
+        menu.classList.add("animationRemove");
+        menu.style.boxShadow = "none";
     }
 });
